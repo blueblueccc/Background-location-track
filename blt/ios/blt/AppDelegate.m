2ea4579f09fef2f6317e73b3b91eecafabe91978
blueblueccc <blueblueccc@yandex.com>
2017-11-02 14:42:13 +0200

    Fix error

--- blt/ios/blt/AppDelegate.m
+++ blt/ios/blt/AppDelegate.m
@@ -19,6 +19,7 @@
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+//  jsCodeLocation = [NSURL URLWithString:@"http://192.168.34.16.xip.io:8081/index.ios.bundle?platform=ios&dev=true"];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"blt"
